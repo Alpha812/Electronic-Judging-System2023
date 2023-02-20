5051f687f6f8dba428803220bf4d186f4da88274
Alpha812 <109557939+Alpha812@users.noreply.github.com>
2023-02-20 12:12:31 +0300

    Create device.py

---
+++ device/device.py
@@ -0,0 +1,13 @@
+from fsm import FSM
+
+
+class Device:
+
+    def __init__(self, fsm=FSM()):
+        self.__fsm = fsm
+        self.__buffer = []
+
+    def is_empty(self):
+        return not len(self.__buffer)
+
+        
